dcc4b3dcb41ce6c2b8c9a1abcd81b021455439b9
ewasem <eduardowasem@outlook.com>
2021-09-30 16:51:11 -0300

    Implementada o menu

--- app/src/main/java/com/ewapps/listacontatosux/MainActivity.kt
+++ app/src/main/java/com/ewapps/listacontatosux/MainActivity.kt
@@ -2,6 +2,10 @@ package com.ewapps.listacontatosux
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -34,5 +38,30 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo menu 2")
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
