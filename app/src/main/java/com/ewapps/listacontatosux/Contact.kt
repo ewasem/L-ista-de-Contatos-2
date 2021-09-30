4810eb1de72f7eaca2379c9f8a51ed05f6d29943
ewasem <eduardowasem@outlook.com>
2021-09-30 15:57:40 -0300

    Inicio projeto

---
+++ app/src/main/java/com/ewapps/listacontatosux/Contact.kt
@@ -0,0 +1,7 @@
+package com.ewapps.listacontatosux
+
+data class Contact(
+    var name: String,
+    var phone: String,
+    var photo: String
+)
---
+++ app/src/main/java/com/ewapps/listacontatosux/ContactAdapter.kt
@@ -0,0 +1,36 @@
+package com.ewapps.listacontatosux
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){
+
+    private val list: MutableList<Contact> = mutableListOf()
+
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhoto: ImageView = itemView.findViewById(R.id.iv_photo)
+        fun bind(contact: Contact) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
